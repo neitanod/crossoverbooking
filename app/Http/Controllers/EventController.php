92391b5c55a6e7b5e19bff673f8c39322c2d3efd
Sebastián Grignoli <grignoli@gmail.com>
2016-08-22 17:32:19 -0300

    Booking form

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Event;
+use App\Venue;
 use App\VenueMap;
 use App\Http\Requests;
 use Illuminate\Http\Request;
@@ -34,12 +35,14 @@ class EventController extends Controller
     public function book($event_id, $stand_id_internal)
     {
         $event = Event::find($event_id)->first();
+        $venue = Venue::find($event->venue_id)->first();
         $venue_map = VenueMap::where(['event_id'=>$event_id])->first();
         $stand = StandRepository::findByInternalId($venue_map->id, $stand_id_internal);
         return view('book',
           [
             'event'=>$event,
             'stand'=>$stand,
+            'venue'=>$venue,
             'venue_map'=>$venue_map,
             'event_id'=>$event_id,
             'stand_id_internal'=>$stand_id_internal
--- resources/views/book.blade.php
+++ resources/views/book.blade.php
@@ -13,28 +13,102 @@
                 <div class="panel-body">
                     <div class="row">
                         <div class="col-xs-12">
-                            <p>{{ $stand['status'] }}<br/></p>
+                            <img ng-src="{{ asset('pics/stand/full/800x600/') }}/{{ $stand->picture }}" class="pull-right stand-picture-big col col-sm-6 img-responsive"/>
+                            <h4>Starting: {{ date('D, F d, Y',strtotime($event->start_date)) }}</h4>
+                            <h4>Until: {{ date('D, F d, Y',strtotime($event->end_date)) }}</h4>
+                            <p>{{ $venue['name'] }}<br/>{{ $venue_map['name'] }}<br/></p>
                             <h3>Stand {{ $stand['id_internal'] }}</h3>
                             <h4>Current status: {{ $stand['status'] }}</h4>
-                            <div id="stand-details" class="row row-spacing" ng-if="selected.id">
-                                <img ng-src="{{ asset('pics/stand/full/800x600/') }}/{| selected.picture |}" class="stand-picture col col-sm-4 img-responsive"/>
-                                <div ng-if="selected.status=='available'" class="col col-sm-8">
-                                    <a id="call-to-action" class="btn btn-danger btn-large btn-lg pull-right" ng-href="{{ URL::to('stand') }}/{| event.id |}/{| selected.id_internal |}">Book this stand</a>
-                                    <h2>Stand {| selected.id_internal |}</h2>
-                                    <h1>Price: <span class="stand-price">$ {| selected.price|number:0 |}</span></h1>
+                            @if($stand->status == 'available')
+                            <h4>Current price: ${{ $stand->price }}</h4>
+                            @endif
+
+                        </div>
+                        <div class="col-xs-10 col-xs-offset-1">
+                            @if($stand->status == 'available')
+
+                            <div class="row">
+
+                            <form method="post" id="form-reservation" data-parsley-validate="">
+
+                              <div class="form-group row row-spacing">
+                                <input id="f_s_id" type="hidden" name="s[id]" value="{{ $stand->id }}">
+                                <input id="csrf_token" type="hidden" name="_token" value="{{ csrf_token() }}">
+                                <label for="f_c_name" class="col-xs-3 col-form-label">Company name</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_name" type="text" name="c[name]" value="" class="form-control" required="">
                                 </div>
-                                <div ng-if="selected.status=='reserved'" class="col col-sm-8">
-                                    <div class="stand-company pull-left">
-                                        Reserved by:
-                                        <h3>{| selected.company |}</h3>
-                                        <div class="row row-spacing" ng-if="selected.documents">
-                                            <div class="col-xs-12" ng-repeat="doc in selected.documents"><a href="{{ URL::to('/documents/') }}/{| doc.file |}" download><i class="fa fa-file"></i> {| doc.title |}</a></div>
-                                        </div>
-                                    </div>
-                                        <img ng-src="{{ asset('pics/company/full/800x600/') }}/{| selected.company_logo |}" class="pull-right stand-company-logo"/>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_logo" class="col-xs-3 col-form-label">Company logo</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_logo" type="hidden" name="c[logo]" value="" class="form-control">
+
+                                  @require('file_uploader')
+
                                 </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_admin_name" class="col-xs-3 col-form-label">Contact person's name</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_admin_name" type="text" name="c[admin_name]" value="" class="form-control">
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_admin_email" class="col-xs-3 col-form-label">Contact person's email</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_admin_email" type="email" name="c[admin_email]" value="" class="form-control">
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_phone" class="col-xs-3 col-form-label">Company's contact phone number</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_phone" type="text" name="c[phone]" value="" class="form-control">
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_email" class="col-xs-3 col-form-label">Company's contact email address</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_email" type="email" name="c[email]" value="" class="form-control">
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_website" class="col-xs-3 col-form-label">Company's Website URL</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_website" type="text" name="c[website]" value="" class="form-control">
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_facebook" class="col-xs-3 col-form-label">Company's Facebook account</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_facebook" type="text" name="c[facebook]" value="" class="form-control">
+                                </div>
+                              </div>
+                              <div class="form-group row">
+                                <label for="f_c_twitter" class="col-xs-3 col-form-label">Company's Twitter account</label>
+                                <div class="col-xs-9">
+                                  <input id="f_c_twitter" type="text" name="c[twitter]" value="" class="form-control">
+                                </div>
+                              </div>
+
+
+                              <div class="form-group row">
+                                <label for="f_c_twitter" class="col-xs-3 col-form-label">Marketing documents</label>
+                                <div class="col-xs-9">
+                                  @require('file_uploader')
+                                <span class="btn btn-primary">Add a document</span>
+                                </div>
+                              </div>
+
+                              <div class="form-group row">
+                                <span class='btn btn-primary btn-lg btn-large pull-right' ng-click="makeReservation()">Confirm reservation</span>
+                              </div>
+
+                            </form>
                             </div>
-                            </div>
+
+                            @endif
+
                         </div>
                     </div>
                 </div>
@@ -46,4 +120,5 @@
 
 @section('content-js')
 <script src="{{ asset('lib/app/ngBookStand.js') }}"></script>
+<script src="{{ asset('lib/parsley-2.4.4/parsley.min.js') }}"></script>
 @endsection
